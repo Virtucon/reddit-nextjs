492ac164c464edf2f4952f7e3a47dc416bd83f13
Virtucon <virtucon5544@gmail.com>
2023-04-09 22:13:48 +0100

    added navbar logo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,11 +1,17 @@
-import { FC } from 'react'
+import { Flex, Image } from "@chakra-ui/react";
+import { FC } from "react";
 
-interface NavbarProps {
-  
-}
+interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
-  return <div>Navbar</div>
-}
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" height="30px" alt="logo" />
+        <Image src="/images/redditText.svg" height="46px" alt="logoText" display={{ base: "none", md: "unset" }} />
+      </Flex>
+    </Flex>
+  );
+};
 
-export default Navbar
+export default Navbar;
