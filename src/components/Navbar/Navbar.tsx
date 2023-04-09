431a3aa1162c3568760a8ca270b3d698ca732ed0
Virtucon <virtucon5544@gmail.com>
2023-04-09 22:42:08 +0100

    chanhed package.json versions and fixed a few deps changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,17 @@
 import { Flex, Image } from "@chakra-ui/react";
 import { FC } from "react";
+import SearchInput from "./SearchInput";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
   return (
-    <Flex bg="white" height="44px" padding="6px 12px">
-      <Flex align="center">
-        <Image src="/images/redditFace.svg" height="30px" alt="logo" />
-        <Image src="/images/redditText.svg" height="46px" alt="logoText" display={{ base: "none", md: "unset" }} />
+    <Flex bg="white" height="44px" padding="6px 12px" justifyContent={{ md: "space-between" }}>
+      <Flex align="center" width={{ base: "40px", md: "auto" }} mr={{ base: 0, md: 2 }} cursor="pointer">
+        <Image src="/images/redditFace.svg" height="30px" alt="text" />
+        <Image display={{ base: "none", md: "unset" }} src="/images/redditText.svg" height="46px" alt="text" />
       </Flex>
+      <SearchInput />
     </Flex>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import { ChakraBaseProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { theme } from "../chakra/theme";
 import Layout from "@/components/layout/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraBaseProvider theme={theme}>
+    <ChakraProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </ChakraBaseProvider>
+    </ChakraProvider>
   );
 }
