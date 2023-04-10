52f25bcd309702accff1ac4ef89c24d03f89ac10
Conor Mather <virtucon5544@gmail.com>
2023-04-10 16:41:04 +0100

    feat: auth button styles (#3)

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,7 +1,5 @@
 import { extendTheme } from "@chakra-ui/react";
-import "@fontsource/open-sans/300.css";
-import "@fontsource/open-sans/400.css";
-import "@fontsource/open-sans/700.css";
+import { Button } from "./button";
 
 export const theme = extendTheme({
   colors: {
@@ -18,5 +16,8 @@ export const theme = extendTheme({
         bg: "gray.200",
       },
     }),
+  },
+  components: {
+    Button,
   },
 });
