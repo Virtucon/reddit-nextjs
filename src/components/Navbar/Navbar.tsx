74a5e64c5384100258564ecaa41eba915b4e38fc
Conor Mather <virtucon5544@gmail.com>
2023-04-11 20:07:24 +0100

    feat: log out #10)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,41 @@
 import { Flex, Image } from "@chakra-ui/react";
 import { FC } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import RightContent from "./rightContent/RightContent";
 import SearchInput from "./SearchInput";
+import { auth } from "../../firebase/clientApp";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
+  const [user, loading, error] = useAuthState(auth);
   return (
-    <Flex bg="white" height="44px" padding="6px 12px" justifyContent={{ md: "space-between" }}>
-      <Flex align="center" width={{ base: "40px", md: "auto" }} mr={{ base: 0, md: 2 }} cursor="pointer">
-        <Image src="/images/redditFace.svg" height="30px" alt="text" />
-        <Image display={{ base: "none", md: "unset" }} src="/images/redditText.svg" height="46px" alt="text" />
+    <Flex
+      bg="white"
+      height="44px"
+      padding="6px 12px"
+      justifyContent={{ md: "space-between" }}
+    >
+      <Flex
+        align="center"
+        width={{ base: "40px", md: "auto" }}
+        mr={{ base: 0, md: 2 }}
+        cursor="pointer"
+      >
+        <Image
+          src="/images/redditFace.svg"
+          height="30px"
+          alt="text"
+        />
+        <Image
+          display={{ base: "none", md: "unset" }}
+          src="/images/redditText.svg"
+          height="46px"
+          alt="text"
+        />
       </Flex>
       <SearchInput />
-      <RightContent />
+      <RightContent user={user} />
     </Flex>
   );
 };
--- src/components/Navbar/rightContent/RightContent.tsx
+++ src/components/Navbar/rightContent/RightContent.tsx
@@ -1,16 +1,27 @@
 import AuthModal from "@/components/modal/auth/AuthModal";
-import { Flex } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
+import { signOut } from "firebase/auth";
 import { FC } from "react";
 import AuthButtons from "./AuthButtons";
+import { auth } from "@/firebase/clientApp";
 
-interface RightContentProps {}
+interface RightContentProps {
+  user: any;
+}
 
-const RightContent: FC<RightContentProps> = ({}) => {
+const RightContent: FC<RightContentProps> = ({ user }) => {
   return (
     <>
       <AuthModal />
-      <Flex justify="center" align="center">
-        <AuthButtons />
+      <Flex
+        justify="center"
+        align="center"
+      >
+        {user ? (
+          <Button onClick={() => signOut(auth)}>Log out</Button>
+        ) : (
+          <AuthButtons />
+        )}
       </Flex>
     </>
   );
