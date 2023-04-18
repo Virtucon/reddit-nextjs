41a015977115ad85828e51dced26237dfdbe82f0
Virtucon <virtucon5544@gmail.com>
2023-04-18 17:36:46 +0100

    sucessfully detects when a user has joined a group

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -1,9 +1,20 @@
-import { Community, communityState } from "@/atoms/communitiesAtom";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { useRecoilState } from "recoil";
+import {
+  Community,
+  CommunitySnippet,
+  communityState,
+} from "../atoms/communitiesAtom";
+import { auth, firestore } from "../firebase/clientApp";
 
 const useCommunityData = () => {
+  const [user] = useAuthState(auth);
   const [communityStateValue, setCommunityStateValue] =
     useRecoilState(communityState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const onJoinOrLeaveCommunity = (
     communityData: Community,
@@ -17,13 +28,37 @@ const useCommunityData = () => {
     joinCommunity(communityData);
   };
 
+  const getMySnippets = async () => {
+    setLoading(true);
+    try {
+      const snippetDocs = await getDocs(
+        collection(firestore, `users/${user?.uid}/communitySnippets`)
+      );
+
+      const snippets = snippetDocs.docs.map((doc) => ({ ...doc.data() }));
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        mySnippets: snippets as CommunitySnippet[],
+      }));
+    } catch (error) {
+      console.log("getMySnippets error", error);
+    }
+    setLoading(false);
+  };
+
   const joinCommunity = (communityData: Community) => {};
 
   const leaveCommunity = (communityId: string) => {};
 
+  useEffect(() => {
+    if (!user) return;
+    getMySnippets();
+  }, [user]);
+
   return {
     communityStateValue,
     onJoinOrLeaveCommunity,
+    loading,
   };
 };
 
